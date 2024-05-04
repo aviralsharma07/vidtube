cb45e5063ae05c404006bb58536471ad59d3c46b
aviralsharma07 <thisisavi200@gmail.com>
2024-05-04 12:39:32 +0530

    create homepage

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import Feed from "../../Components/Feed/Feed";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import "./Home.css";
+
+const Home = ({ sidebar }) => {
+  return (
+    <>
+      <Sidebar sidebar={sidebar} />
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed />
+      </div>
+    </>
+  );
+};
+
+export default Home;
