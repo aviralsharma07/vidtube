e56c409eb6cf204625c8b7aeb833216abf28e9a3
aviralsharma07 <thisisavi200@gmail.com>
2024-05-05 11:10:18 +0530

    Frontend complete

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,12 @@
 import "./Video.css";
 import PlayVideo from "../../Components/PlayVideo/PlayVideo";
+import Recommended from "../../Components/Recommended/Recommended";
 
 const Video = () => {
   return (
     <div className="play-container">
       <PlayVideo />
+      <Recommended />
     </div>
   );
 };
